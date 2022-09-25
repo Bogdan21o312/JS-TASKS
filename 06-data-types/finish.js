755c31b43f7b2a8ad89f6c9c8881b45fe5ffc0db
Bogdan21o312 <lutskiv2010@gmail.com>
2022-09-25 17:57:29 +0300

    Task 6 completed

--- 06-data-types/finish.js
+++ 06-data-types/finish.js
@@ -12,3 +12,28 @@
  * 2. Выведите в консоль тип каждого из значений
  * используя оператор typeof
  */
+
+const a = 'String'
+
+const b = 1
+
+const c = true
+
+const v = null
+
+let d
+
+const i = {
+    id: 0,
+    name: 'Bohdan'
+}
+
+const f = [1, 2, 3]
+
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
+console.log(typeof v)
+console.log(typeof d)
+console.log(typeof i)
+console.log(typeof f)
