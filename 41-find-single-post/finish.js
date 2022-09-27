7bc28de4088568b11f425630b892aa6f66f35ae3
Bogdan21o312 <lutskiv2010@gmail.com>
2022-09-27 13:40:12 +0300

    Task 41 completed

--- 41-find-single-post/finish.js
+++ 41-find-single-post/finish.js
@@ -18,6 +18,12 @@ const posts = [
   { postId: 6134, commentsQuantity: 2 },
 ]
 
+const findPostById = (postId, posts) => {
+  return  posts.find((posts) => posts.postId === postId)
+}
+
 console.log(findPostById(6134, posts)) // { postId: 6134, commentsQuantity: 2 }
 
 console.log(findPostById(4511, posts)) // undefined
+
+
